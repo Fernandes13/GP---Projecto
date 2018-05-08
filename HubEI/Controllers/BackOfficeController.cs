ea8c59daef86f1870c9e18f034485f42a84d75b1
BrunoMPereira <brunop.esac@hotmail.com>
2018-05-08 21:39:13 +0100

    Continued Students CRUD

--- HubEI/Controllers/BackOfficeController.cs
+++ HubEI/Controllers/BackOfficeController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HubEI.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HubEI.Controllers
 {
     public class BackOfficeController : Controller
     {
+        private readonly HUBEI_DBContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public BackOfficeController(HUBEI_DBContext context, IHostingEnvironment HostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = HostingEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,7 +27,14 @@ namespace HubEI.Controllers
 
         public IActionResult Students()
         {
-            return View();
+
+            var students = from s
+                           in _context.Student
+                           select s;
+
+            //return await PaginatedList<Technician>.CreateAsync(technicians.AsNoTracking(), intTechniciansPageNumber, intPendingPageSize);
+            return View(students);
+
         }
 
     }
